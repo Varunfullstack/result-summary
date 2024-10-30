0c82a5ffde06ad6705b9264e2e5f72e476f86d4b
Varun <varunphp.dev@gmail.com>
2024-10-30 19:32:28 +0530

    feat: add new assets and configure Tailwind and Next.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Primary colors
+        red: "hsl(0, 100%, 67%)",
+        yellow: "hsl(39, 100%, 56%)",
+        green: "hsl(166, 100%, 37%)",
+        blue: "hsl(234, 85%, 45%)",
+      },
+      fontSize: {
+        body: "18px",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      fontFamily: {
+        sans: ["var(--font-hanken)"],
+      },
+      boxShadow: {
+        card: "0 8px 25px -4px rgb(233,237,252), 0 0 25px -10px rgb(233,237,252)",
+      },
+      borderRadius: {
+        card: "32px",
+      },
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+        extrabold: 800,
       },
     },
   },
